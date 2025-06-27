66c87c44b1064f0909b924d0f294c0beff386f5a
MaximilianoAcevedo <ma.acevedok@duocuc.cl>
2025-06-26 21:32:57 -0400

    Agregue funcion de Maximiliano

--- menu_base.py
+++ menu_base.py
@@ -1,3 +1,5 @@
+def datos_maximiliano():
+    print("Mi nombre es Maximiliano Acevedo y tengo 22 años")
 # Menú base del programa
 while True:
     print("\n--- MENÚ PRINCIPAL ---")
@@ -12,7 +14,7 @@ while True:
         print("Programa finalizado.")
         break
     elif op == "1":
-        pass # Aquí se llamará a la función del integrante 1
+        datos_maximiliano()
     elif op == "2":
         pass # Aquí se llamará a la función del integrante 2
     elif op == "3":
